fix(solve_g): fix upper matrix composition in solve_g, finish g function calculation

hdr.py
@@ -1,4 +1,5 @@
 import math
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -17,6 +18,7 @@ class HDR():
         self.EXTS = ['.jpg', '.png', '.jpeg']
         self.MAX_VAL = 255
         self.MIN_VAL = 0
+        self.z_range_length = self.MAX_VAL - self.MIN_VAL + 1
 
         # image count
         self.P = 0
@@ -24,20 +26,32 @@ class HDR():
         # sample pixel count
         self.N = n
 
-        # list of g functions in r, g, b order
-        self.G: List[np.ndarray] = []
+        # image size
+        self.W = 0
+        self.H = 0
 
         self.L: float = lc
 
+        self.ch_num = 3
+
+        # list of g functions in r, g, b order
+        self.G = np.zeros((self.ch_num, self.z_range_length))
+
         # Images as numpy array
         self.imgs: List[np.ndarray] = []
-        self.ch_r: List[np.ndarray] = []
-        self.ch_g: List[np.ndarray] = []
-        self.ch_b: List[np.ndarray] = []
 
         # List of log(Exposure time)
         self.exposure_time: List[float] = []
         self.load_folder(img_folder)
+
+        # rearrange image layers channel-wisely
+        # shape: [channels, images num, height, width]
+        self.layers = np.zeros((self.ch_num, self.P, self.H, self.W), dtype=np.uint8)
+        for i in range(self.ch_num):
+            self.layers[i] = np.array([img[:, :, i] for img in self.imgs])
+            # np.savetxt('sample_a.txt', self.layers[i][0])
+        self.samples = np.zeros((self.ch_num, self.N, self.P), dtype=np.uint8)
+        self.sample()
 
     # TODO Add size check
     def load_folder(self, path: str):
@@ -60,17 +74,40 @@ class HDR():
 
             self.exposure_time.append(math.log10(d_t))
             np_img = np.array(img)
+            self.H = np_img.shape[0]
+            self.W = np_img.shape[1]
 
             self.imgs.append(np_img)
 
-            step = math.ceil(np_img.shape[0] * np_img.shape[1] / self.N)
+        self.P = len(self.imgs)
 
-            # separates channels -> reshapes to 1D -> samples N points
-            self.ch_r.append(np_img[:, :, 0].ravel()[0:-1:step])
-            self.ch_g.append(np_img[:, :, 1].ravel()[0:-1:step])
-            self.ch_b.append(np_img[:, :, 2].ravel()[0:-1:step])
+    @staticmethod
+    def load_image(path: str) -> Image.Image:
+        return Image.open(path).convert('RGB')
 
-            self.P += 1
+    def read_exposure_time(self, img: Image.Image) -> float:
+        exif = exif_utils.parse_exif(img)
+        return float(exif[self.EXPOSURE_TIME_KEY])
+
+    def sample(self):
+
+        step = math.ceil((self.MAX_VAL - self.MIN_VAL + 1) / self.N)
+
+        for ch_idx in range(self.ch_num):
+            sample_layer = self.layers[ch_idx, self.P // 2, :, :]
+
+            # randomly samples from points in sample_layer where value == z
+            sample_idx = 0
+            for z in range(self.MIN_VAL, self.MAX_VAL, step):
+                ys, xs = np.where(sample_layer == z)
+
+                if len(ys) == 0:
+                    continue
+
+                sample_pos_idx = random.randrange(len(ys))
+
+                self.samples[ch_idx][sample_idx] = self.layers[ch_idx, :, ys[sample_pos_idx], xs[sample_pos_idx]]
+                sample_idx += 1
 
     # W func
     def weighting(self, val: float) -> float:
@@ -90,74 +127,61 @@ class HDR():
 
         return val - self.MIN_VAL
 
-    @staticmethod
-    def load_image(path: str) -> Image.Image:
-        return Image.open(path).convert('RGB')
-
-    def read_exposure_time(self, img: Image.Image) -> float:
-        exif = exif_utils.parse_exif(img)
-        return float(exif[self.EXPOSURE_TIME_KEY])
-
     def solve_g(self):
 
         z_range_length = self.MAX_VAL - self.MIN_VAL + 1
+        row_num = self.N * self.P + 1 + z_range_length - 2
+        col_num = z_range_length + self.N
 
-        for ch in [self.ch_r, self.ch_g, self.ch_b]:
-            row_num = self.N * self.P + 1 + z_range_length - 2
-            col_num = z_range_length + self.N
-            a = np.zeros((row_num, col_num))
-            b = np.zeros((row_num))
+        for ch in range(self.ch_num):
+
+            mat_a = np.zeros((row_num, col_num))
+            mat_b = np.zeros((row_num))
 
             row_idx = 0
+
             # composes upper part
-            for pic_idx, p in enumerate(ch):
-                for z in p:
+            for sample_idx, samples in enumerate(self.samples[ch]):
+                for pic_idx, z in enumerate(samples):
                     w_z = self.weighting(z)
-                    a[row_idx][z] = 1 * w_z
-                    a[row_idx][z_range_length + pic_idx] = -1 * w_z
-                    b[row_idx] = self.exposure_time[pic_idx] * w_z
+                    mat_a[row_idx][z] = 1 * w_z
+                    mat_a[row_idx][z_range_length + sample_idx] = -1 * w_z
+                    mat_b[row_idx] = self.exposure_time[pic_idx] * w_z
                     row_idx += 1
 
             # add constraint for g_mid == 0
-            a[row_idx][(z_range_length) // 2] = 1
+            mat_a[row_idx][(z_range_length) // 2] = 1
             # b[row_idx] = 0
             row_idx += 1
 
             # composes lower part
             for i in range(1, z_range_length - 1):
                 w_z = self.weighting(i)
-                a[row_idx][i - 1] = 1 * self.L * w_z
-                a[row_idx][i] = -2 * self.L * w_z
-                a[row_idx][i + 1] = 1 * self.L * w_z
+                mat_a[row_idx][i - 1] = 1 * self.L * w_z
+                mat_a[row_idx][i] = -2 * self.L * w_z
+                mat_a[row_idx][i + 1] = 1 * self.L * w_z
                 # b[row_idx] = 0
                 row_idx += 1
 
             # solves Ax = b
-            # np.savetxt('B.txt', b)
-            # x = np.linalg.lstsq(a, b)
+            x = np.linalg.lstsq(mat_a, mat_b)
 
-            inv_a = np.linalg.pinv(a)
-            x = np.dot(inv_a, b)
-
-            g = x[0: z_range_length]
-            print(g)
-            self.G.append(g)
-            # self.G.append(x[0][0:z_range_length])
-            # print(len(x[0]), x[0][self.MIN_VAL:self.MAX_VAL + 1])
+            self.G[ch] = x[0][self.MIN_VAL:self.MAX_VAL + 1]
 
     def plot_g(self):
         label_list = ['R', 'G', 'B']
+        marker_list = ['r--', 'g', 'b']
         for idx, data in enumerate(self.G):
             plt.subplot(2, 2, idx + 1)
             plt.title(label_list[idx])
-            plt.plot(data[:])
+            plt.plot(data[:], marker_list[idx])
             plt.xticks([])
             plt.yticks([])
+
+            plt.subplot(2, 2, 4)
+            plt.title(label_list[idx])
+            plt.plot(data[:], marker_list[idx])
+            plt.xticks([])
+            plt.yticks([])
+
         plt.show()
-
-    # def compute_radiance_map(self) -> float:
-
-    #     m = np.array([])
-    #     for idx, img in enumerate(self.imgs):
-    #         for ch in range(3):
-    #             m = np.append(m, self.weighting(self.g(img) - self.exposure_time[idx]))